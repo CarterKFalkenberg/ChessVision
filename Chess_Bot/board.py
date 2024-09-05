997f92931e54c45e6f0970ceb4f17b37c638842f
Carter Karl Falkenberg <ckfalk21@gmail.com>
2024-09-04 20:24:12 -0400

    created board class

--- Chess_Bot/board.py
+++ Chess_Bot/board.py
@@ -2,10 +2,39 @@ import numpy as np
 
 class board:
     def __init__(self):
-        self.board = np.zeros((8, 8), dtype=np.uint8)
+        """Creates 8x8 board representation
+        Empty is represented as X
+        Piece is represented by 2 chars: Color (W/B) and Piece:
+            P : Pawn
+            R : Rook
+            N : Knight
+            B : Bishop
+            Q : Queen 
+            K : King
+        """
+        board = np.zeros((8, 8))
         
+        # PAWNS
+        board[8:15] = ["BP" for i in range(8)]
+        board[32:39] = ["WP" for i in range(8)]
+        
+        # ROOKS
+        
+        # KNIGHTS
+        
+        # BISHOPS
+        board[2], board[5] = "BB", "BB"
+        board[42], board[45] = "BB", "BB"
+        
+        # QUEENS 
+        
+        # KINGS
+         
     def possible_moves(self):
-        """Returns a list of all possible moves in the board's position"""
+        """Returns a list of all possible moves in the board's position
+        A move is a 2-tuple, containing the piece (e.g. "WP") and the square's index (e.g. (2, 4))
+            e.g. move = ("WP", (2, 4))      # white's pawn to C4
+        """
         pass
     
     def make_move(self, piece, row, column):
