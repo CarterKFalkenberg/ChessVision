0c7270137f7e075abba8c23b3d9056e3c1e0a9c9
Carter Karl Falkenberg <ckfalk21@gmail.com>
2024-09-08 15:24:02 -0400

    piece and color constants

---
+++ Chess_Bot/Constants.java
@@ -0,0 +1,17 @@
+package Chess_Bot;
+
+public final class Constants {
+    public static final int BLACK = -1;
+    public static final int WHITE = 1;
+    public static final int EMPTY = 0;
+    public static final int PAWN = 1;
+    public static final int KNIGHT = 2;
+    public static final int BISHOP = 3;
+    public static final int ROOK = 4;
+    public static final int QUEEN = 5;
+    public static final int KING = 6;
+
+    private Constants() {
+    // Prevent instantiation
+    }
+ }
