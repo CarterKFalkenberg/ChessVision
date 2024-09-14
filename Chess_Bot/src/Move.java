47f71e190ae267f71a8947b81343df09b5578cd6
Carter Karl Falkenberg <ckfalk21@gmail.com>
2024-09-13 20:21:09 -0400

    moved files

---
+++ Chess_Bot/src/Move.java
@@ -0,0 +1,16 @@
+package Chess_Bot.src;
+
+import Chess_Bot.src.Pieces.Piece;
+
+public class Move {
+    
+    Piece piece; 
+    int start_row;
+    int start_column;
+    int end_row;
+    int end_column;
+
+    Move(){
+
+    }
+}
