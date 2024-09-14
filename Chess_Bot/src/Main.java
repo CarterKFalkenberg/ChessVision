f194d11391db438c3d3f12a0ea2219391ac6bc75
Carter Karl Falkenberg <ckfalk21@gmail.com>
2024-09-13 20:20:59 -0400

    init main file

---
+++ Chess_Bot/src/Main.java
@@ -0,0 +1,9 @@
+package Chess_Bot.src;
+
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Board board = new Board();
+        System.out.println(board);
+    }
+}
