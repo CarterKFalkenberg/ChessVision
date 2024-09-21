7797a085bc05f1229ca7611caf186c5629753a94
Carter Karl Falkenberg <ckfalk21@gmail.com>
2024-09-21 12:17:54 -0400

    created constructor

--- Chess_Bot/src/Move.java
+++ Chess_Bot/src/Move.java
@@ -10,7 +10,11 @@ public class Move {
     int end_row;
     int end_column;
 
-    Move(){
-
+    public Move(Piece piece, int start_row, int start_column, int end_row, int end_column){
+        this.piece = piece;
+        this.start_row = start_row;
+        this.start_column = start_column;
+        this.end_row = end_row;
+        this.end_column = end_column;
     }
 }
