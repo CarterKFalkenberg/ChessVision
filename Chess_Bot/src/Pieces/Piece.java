1b69f25be92b4fa1b0c9439fca8e5fef4aa4121e
Carter Karl Falkenberg <ckfalk21@gmail.com>
2024-09-28 18:00:18 -0400

    fixed get moves logic, cleaned code

--- Chess_Bot/src/Pieces/Piece.java
+++ Chess_Bot/src/Pieces/Piece.java
@@ -33,66 +33,48 @@ public class Piece {
 
     public ArrayList<Move> getStraightPathMoves(int[][] boardColors){
         // starting from our square, check in all 4 directions, repeat until all of them hit either a piece or end of board
-        boolean checkForwards = this.square[0] < 7;
         int forwardRow = this.square[0] + 1;
-
-        boolean checkBackwards = this.square[0] > 0;
         int backwardRow = this.square[0] - 1;
-
-        boolean checkRight = this.square[1] < 7;
-        int rightRow = this.square[1] + 1;
-
-        boolean checkLeft = this.square[1] > 0;
-        int leftRow = this.square[1] - 1;
-        
+        int rightCol = this.square[1] + 1;
+        int leftCol = this.square[1] - 1;
         
         ArrayList<Move> straightPathMoves = new ArrayList<Move>();
 
-        while (checkForwards && checkBackwards && checkLeft && checkRight){
-            if (checkForwards){
-                if (boardColors[forwardRow][this.square[1]] == pieceColor){
-                    checkForwards = false;
-                } else {
-                    straightPathMoves.add(new Move(this, this.square[0], this.square[1], forwardRow, this.square[1]));
-                    if (boardColors[forwardRow][this.square[1]] != Constants.EMPTY){
-                        checkForwards = false;
-                    } 
-                }
-                forwardRow++;
-            }
-            if (checkBackwards){
-                if (boardColors[backwardRow][this.square[1]] == pieceColor){
-                    checkBackwards = false;
-                } else {
-                    straightPathMoves.add(new Move(this, this.square[0], this.square[1], backwardRow, this.square[1]));
-                    if (boardColors[backwardRow][this.square[1]] != Constants.EMPTY){
-                        checkBackwards = false;
-                    } 
-                }
-                backwardRow--;
-            }
-            if (checkRight){
-                if (boardColors[this.square[0]][rightRow] == pieceColor){
-                    checkRight = false;
-                } else {
-                    straightPathMoves.add(new Move(this, this.square[0], this.square[1], this.square[0], rightRow));
-                    if (boardColors[this.square[0]][rightRow] != Constants.EMPTY){
-                        checkRight = false;
-                    } 
-                }
-                rightRow++;
-            }
-            if (checkLeft){
-                if (boardColors[this.square[0]][leftRow] == pieceColor){
-                    checkLeft = false;
-                } else {
-                    straightPathMoves.add(new Move(this, this.square[0], this.square[1], this.square[0], leftRow));
-                    if (boardColors[this.square[0]][leftRow] != Constants.EMPTY){
-                        checkLeft = false;
-                    } 
-                }
-                leftRow--;
-            }
+        while(forwardRow < 8 && boardColors[forwardRow][this.square[1]] != this.pieceColor){    
+            straightPathMoves.add(new Move(this, this.square[0], this.square[1], forwardRow, this.square[1]));
+            // forwardRow is our opponent's color -> stop checking
+            if (boardColors[forwardRow][this.square[1]] != Constants.EMPTY){
+                break; 
+            } 
+            // forwardRow is empty -> keep checking diagonal
+            forwardRow++;
+        }
+        while(backwardRow >= 0 && boardColors[backwardRow][this.square[1]] != this.pieceColor){    
+            straightPathMoves.add(new Move(this, this.square[0], this.square[1], backwardRow, this.square[1]));
+            // backwardRow is our opponent's color -> stop checking
+            if (boardColors[backwardRow][this.square[1]] != Constants.EMPTY){
+                break; 
+            } 
+            // backwardRow is empty -> keep checking diagonal
+            backwardRow--;
+        }
+        while(rightCol < 8 && boardColors[this.square[0]][rightCol] != this.pieceColor){    
+            straightPathMoves.add(new Move(this, this.square[0], this.square[1], this.square[0], rightCol));
+            // rightCol is our opponent's color -> stop checking
+            if (boardColors[this.square[0]][rightCol] != Constants.EMPTY){
+                break; 
+            } 
+            // rightCol is empty -> keep checking diagonal
+            rightCol++;
+        }
+        while(leftCol >= 0 && boardColors[this.square[0]][leftCol] != this.pieceColor){    
+            straightPathMoves.add(new Move(this, this.square[0], this.square[1], this.square[0], leftCol));
+            // leftCol is our opponent's color -> stop checking
+            if (boardColors[this.square[0]][leftCol] != Constants.EMPTY){
+                break; 
+            } 
+            // leftCol is empty -> keep checking diagonal
+            leftCol--;
         }
 
         return straightPathMoves;
@@ -100,75 +82,63 @@ public class Piece {
 
     public ArrayList<Move> getDiagonalMoves(int[][] boardColors){
         // starting from our square, check in all 4 directions, repeat until all of them hit either a piece or end of board
-        boolean checkFrontRight = this.square[0] < 7 && this.square[1] < 7;
         int[] frontRightSquare = {this.square[0] + 1, this.square[1] + 1};
-
-        boolean checkFrontLeft = this.square[0] < 7 && this.square[1] > 0;
         int[] frontLeftSquare = {this.square[0] + 1, this.square[1] - 1};
-
-        boolean checkBackRight = this.square[0] > 0 && this.square[1] < 7;
         int[] backRightSquare = {this.square[0] - 1, this.square[1] + 1};
-        
-        boolean checkBackLeft = this.square[0] > 0 && this.square[1] > 0;
         int[] backLeftSquare = {this.square[0] - 1, this.square[1] - 1};
         
         ArrayList<Move> diagonalPathMoves = new ArrayList<Move>();
 
-        while (checkFrontRight && checkFrontLeft && checkBackRight && checkBackLeft){
-            if (checkFrontRight){
-                if (boardColors[frontRightSquare[0]][frontRightSquare[1]] == pieceColor){
-                    checkFrontRight = false;
-                } else {
-                    diagonalPathMoves.add(new Move(this, this.square[0], this.square[1], frontRightSquare[0], frontRightSquare[1]));
-                    if (boardColors[frontRightSquare[0]][frontRightSquare[1]] != Constants.EMPTY){
-                        checkFrontRight = false;
-                    } 
-                }
-                frontRightSquare[0]++;
-                frontRightSquare[1]++;
-            }
-            if (checkFrontLeft){
-                if (boardColors[frontLeftSquare[0]][frontLeftSquare[0]] == pieceColor){
-                    checkFrontLeft = false;
-                } else {
-                    diagonalPathMoves.add(new Move(this, this.square[0], this.square[1], frontLeftSquare[0], frontLeftSquare[0]));
-                    if (boardColors[frontLeftSquare[0]][frontLeftSquare[0]] != Constants.EMPTY){
-                        checkFrontLeft = false;
-                    } 
-                }
-                frontLeftSquare[0]++;
-                frontLeftSquare[1]--;
-            }
-            if (checkBackRight){
-                if (boardColors[backRightSquare[0]][backRightSquare[1]] == pieceColor){
-                    checkBackRight = false;
-                } else {
-                    diagonalPathMoves.add(new Move(this, this.square[0], this.square[1], backRightSquare[0], backRightSquare[1]));
-                    if (boardColors[backRightSquare[0]][backRightSquare[1]] != Constants.EMPTY){
-                        checkBackRight = false;
-                    } 
-                }
-                backRightSquare[0]--;
-                backRightSquare[1]++;
-            }
-            if (checkBackLeft){
-                if (boardColors[backLeftSquare[0]][backLeftSquare[1]] == pieceColor){
-                    checkBackLeft = false;
-                } else {
-                    diagonalPathMoves.add(new Move(this, this.square[0], this.square[1], backLeftSquare[0], backLeftSquare[1]));
-                    if (boardColors[backLeftSquare[0]][backLeftSquare[1]] != Constants.EMPTY){
-                        checkBackLeft = false;
-                    } 
-                }
-                backLeftSquare[0]--;
-                backLeftSquare[1]++;
-            }
+        while(frontRightSquare[0] < 8 
+                && frontRightSquare[1] < 8 
+                && boardColors[frontRightSquare[0]][frontRightSquare[1]] != this.pieceColor){    
+            diagonalPathMoves.add(new Move(this, this.square[0], this.square[1], frontRightSquare[0], frontRightSquare[1]));
+            // frontRightSquare is our opponent's color -> stop checking
+            if (boardColors[frontRightSquare[0]][frontRightSquare[1]] != Constants.EMPTY){
+                break; 
+            } 
+            // frontRightSquare is empty -> keep checking diagonal
+            frontRightSquare[0]++;
+            frontRightSquare[1]++;
+        }
+        while(frontLeftSquare[0] < 8 
+                && frontLeftSquare[1] >= 0 
+                && boardColors[frontLeftSquare[0]][frontLeftSquare[1]] != this.pieceColor){    
+            diagonalPathMoves.add(new Move(this, this.square[0], this.square[1], frontLeftSquare[0], frontLeftSquare[1]));
+            // frontLeftSquare is our opponent's color -> stop checking
+            if (boardColors[frontLeftSquare[0]][frontLeftSquare[1]] != Constants.EMPTY){
+                break; 
+            } 
+            // frontLeftSquare is empty -> keep checking diagonal
+            frontLeftSquare[0]++;
+            frontLeftSquare[1]--;
+        }
+        while(backRightSquare[0] >= 0
+                && backRightSquare[1] < 8 
+                && boardColors[backRightSquare[0]][backRightSquare[1]] != this.pieceColor){    
+            diagonalPathMoves.add(new Move(this, this.square[0], this.square[1], backRightSquare[0], backRightSquare[1]));
+            // backRightSquare is our opponent's color -> stop checking
+            if (boardColors[backRightSquare[0]][backRightSquare[1]] != Constants.EMPTY){
+                break; 
+            } 
+            // frontRightSquare is empty -> keep checking diagonal
+            backRightSquare[0]--;
+            backRightSquare[1]++;
+        }
+        while(backLeftSquare[0] >= 0
+                && backLeftSquare[1] >= 0 
+                && boardColors[backLeftSquare[0]][backLeftSquare[1]] != this.pieceColor){    
+            diagonalPathMoves.add(new Move(this, this.square[0], this.square[1], backLeftSquare[0], backLeftSquare[1]));
+            // backLeftSquare is our opponent's color -> stop checking
+            if (boardColors[backLeftSquare[0]][backLeftSquare[1]] != Constants.EMPTY){
+                break; 
+            } 
+            // backLeftSquare is empty -> keep checking diagonal
+            backLeftSquare[0]--;
+            backLeftSquare[1]--;
         }
 
         return diagonalPathMoves;
     }
-  
 
-
-    
 }
