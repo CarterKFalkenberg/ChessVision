0e57caaff250aab83b04fa6c5f32aa3f23b65d3e
Carter Karl Falkenberg <ckfalk21@gmail.com>
2024-10-08 16:39:58 -0400

    added promotion and en passant square information

--- Chess_Bot/src/Move.java
+++ Chess_Bot/src/Move.java
@@ -9,6 +9,8 @@ public class Move {
     int start_column;
     int end_row;
     int end_column;
+    boolean promotion;
+    int[] enPassantSquare;
 
     public Move(Piece piece, int start_row, int start_column, int end_row, int end_column){
         this.piece = piece;
@@ -16,5 +18,11 @@ public class Move {
         this.start_column = start_column;
         this.end_row = end_row;
         this.end_column = end_column;
+        this.promotion = piece.getType() == Constants.PAWN && end_row == 0 || end_row == 7;
+        if (piece.getType() == Constants.PAWN && Math.abs(start_row - end_row) == 2){
+            this.enPassantSquare = new int[]{end_row, end_column};
+        } else {
+            this.enPassantSquare = new int[]{-1, -1};
+        }
     }
 }
