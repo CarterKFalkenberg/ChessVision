ed54fe5536117b897d76136ed45c499e1a187a83
Carter Karl Falkenberg <ckfalk21@gmail.com>
2024-10-08 17:39:12 -0400

    added piece value's for evaluation

--- Chess_Bot/src/Constants.java
+++ Chess_Bot/src/Constants.java
@@ -1,5 +1,7 @@
 package Chess_Bot.src;
 
+import java.util.HashMap;
+
 public final class Constants {
     public static final int BLACK = -1;
     public static final int WHITE = 1;
@@ -11,7 +13,22 @@ public final class Constants {
     public static final int QUEEN = 5;
     public static final int KING = 6;
 
+    public static HashMap<Integer, Integer> pieceToValue = initPieceToValue();
+
     private Constants() {
     // Prevent instantiation
     }
+
+    private static HashMap<Integer, Integer> initPieceToValue(){
+        HashMap<Integer, Integer> pieceToValue = new HashMap<Integer, Integer>();
+        pieceToValue.put(PAWN, 1);
+        pieceToValue.put(KNIGHT, 3);
+        pieceToValue.put(BISHOP, 3);
+        pieceToValue.put(ROOK, 5);
+        pieceToValue.put(QUEEN, 9);
+        pieceToValue.put(KING, 0);
+        
+        return pieceToValue;
+    }
+
  }
