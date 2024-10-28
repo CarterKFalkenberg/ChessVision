1d00d61ffadd8e121c5243acb6233df5e59bfa47
Carter Karl Falkenberg <ckfalk21@gmail.com>
2024-10-28 13:09:04 -0400

    testing

--- Chess_Bot/src/Main.java
+++ Chess_Bot/src/Main.java
@@ -1,9 +1,14 @@
 package Chess_Bot.src;
 
 import java.io.IOException;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws IOException{
         Board board = new Board();
-        System.out.println(board);
+        board.turnColor = -1;
+        ArrayList<Move> moves = board.getPossibleMoves();
+        for (Move move : moves){
+            System.out.println(move);
+        }
     }
 }
