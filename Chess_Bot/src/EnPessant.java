502da4822232894a7f40fa05f00122138f320d8a
Carter Karl Falkenberg <ckfalk21@gmail.com>
2024-11-10 11:29:31 -0500

    init en pessant move class

---
+++ Chess_Bot/src/EnPessant.java
@@ -0,0 +1,23 @@
+package Chess_Bot.src;
+
+import Chess_Bot.src.Pieces.Piece;
+
+public class EnPessant extends Move {
+    
+    Piece pawnTaken;
+
+    public EnPessant(Piece piece, int start_row, int start_column, int end_row, int end_column, Piece pawnTaken){
+        super(piece, start_row, start_column, end_row, end_column);
+        this.promotion = false;
+        this.enPassantSquare = new int[]{-1, -1};
+        this.pawnTaken = pawnTaken;
+    }
+
+    @Override
+    public String toString() {
+        // e.g. "WPawn on B3 to B4, en pessant"
+        String str = super.toString();
+        str += ", en pessant";
+        return str;
+    }
+}
