37efe39f45eebb1062e0aee2523f542e4302fb38
Carter Karl Falkenberg <ckfalk21@gmail.com>
2024-11-16 13:57:52 -0500

    add castling support to getPossibleMoves

--- Chess_Bot/src/Pieces/King.java
+++ Chess_Bot/src/Pieces/King.java
@@ -1,13 +1,19 @@
 package Chess_Bot.src.Pieces;
 import java.util.ArrayList;
 
+import Chess_Bot.src.Castle;
 import Chess_Bot.src.Constants;
 import Chess_Bot.src.Move;
 
 public class King extends Piece{
 
-    public King(boolean isWhite, int[] square) {
+    boolean shortCastleRights;
+    boolean longCastleRights;
+
+    public King(boolean isWhite, int[] square, boolean shortCastleRights, boolean longCastleRights) {
         super(isWhite, square);
+        this.shortCastleRights = shortCastleRights;
+        this.longCastleRights = longCastleRights;
     }
 
     @Override
@@ -17,12 +23,57 @@ public class King extends Piece{
 
     @Override
     public ArrayList<Move> getPossibleMoves(int[][] boardColors, int[] enPassantSquare){
+        // returns are theoretically possible moves
         // DOES NOT TAKE INTO ACCOUNT CHECKS OR ILLEGAL MOVING INTO A CHECK! DO THAT DURING GAME LOGIC
+        
         ArrayList<Move> moves = new ArrayList<Move>();
 
-        // TODO: castle short
+        // first, check for castling. Castling is possible if you have rights and there are no pieces in the way
 
-        // TODO: castle long
+        // castle short
+        if (this.shortCastleRights){
+            boolean safe = true;
+
+            // make sure there are no pieces between the king and the rook
+            if (this.isWhite){
+                if (boardColors[7][5] != Constants.EMPTY || boardColors[7][6] != Constants.EMPTY){
+                    safe = false;
+                }
+            } else {
+                if (boardColors[0][5] != Constants.EMPTY || boardColors[0][6] != Constants.EMPTY){
+                    safe = false;
+                }
+            }
+
+            if (safe){
+                moves.add(
+                    new Castle(this, true)
+                );
+            }
+        }
+
+        // castle long
+        if (this.longCastleRights){
+            boolean safe = true;
+
+            // make sure there are no pieces between the king and the rook
+            if (this.isWhite){
+                if (boardColors[7][1] != Constants.EMPTY || boardColors[7][2] != Constants.EMPTY || boardColors[7][3] != Constants.EMPTY){
+                    safe = false;
+                }
+            } else {
+                if (boardColors[0][1] != Constants.EMPTY || boardColors[0][2] != Constants.EMPTY || boardColors[0][3] != Constants.EMPTY){
+                    safe = false;
+                }
+            }
+
+            if (safe){
+                moves.add(
+                    new Castle(this, false)
+                );
+            }
+        }
+
 
         // forward
         if (this.square[0] < 7 && boardColors[this.square[0]+1][this.square[1]] != this.pieceColor) {
